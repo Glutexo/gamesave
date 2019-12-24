78dbe755bd0d4d490a09ccffc3b84dcfd1f4149e
Štěpán Tomsa <stomsa@redhat.com>
2019-12-24 23:43:32 +0100

    Decouple data paths from filesystem
    There is no reason why should SteamFile depend of Filesystem. Joining
    with the user home path can be done from the outside.

--- lib/gamesave/steam_files.rb
+++ lib/gamesave/steam_files.rb
@@ -1,13 +1,9 @@
-require(File.join('gamesave', 'filesystem'))
-
 module Gamesave
   module SteamFiles
     USER_DATA_PATH = File.join('Library', 'Application Support', 'Steam', 'userdata')
     
-    def user_data_path(account_id = nil)
-      path = File.join(Filesystem::home_path, USER_DATA_PATH)
-      path = path + account_id unless account_id.nil?
-      File.join(*path)
+    def user_data_path(account_id)
+      File.join(USER_DATA_PATH, account_id)
     end
     
     def game_data_path(account_id, game_id)
