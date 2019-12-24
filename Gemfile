ac47e45833e2ed565d32daad8780b2c618250619
Štěpán Tomsa <stomsa@redhat.com>
2019-12-24 23:06:32 +0100

    Test bitmask
    Add first tests, testing the bitmask function.

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source('https://rubygems.org')
+
+gem('rspec', '~>3.9.0')
+gem('rspec-parameterized', '~>0.4.2')
--- lib/gamesave/bitwise.rb
+++ lib/gamesave/bitwise.rb
@@ -1,12 +1,12 @@
 module Gamesave
   module Bitwise
+    def bitmask(length)
+      2 ** length - 1
+    end
+
     def first_bits(data, length)
       mask = bitmask(length)
       data & mask
-    end
-    
-    def bitmask(length)
-      2 ** length - 1
     end
     
     module_function(:first_bits, :bitmask)
