e60884b49d5562b2235ac58104ecb62b59c105aa
Štěpán Tomsa <stomsa@redhat.com>
2019-12-24 23:39:14 +0100

    Rename User ID to Account ID
    “Account ID” is used to describe the 32-bit Steam ID. Renamed other
    names to Account ID.

--- lib/gamesave/steam_files.rb
+++ lib/gamesave/steam_files.rb
@@ -4,14 +4,14 @@ module Gamesave
   module SteamFiles
     USER_DATA_PATH = File.join('Library', 'Application Support', 'Steam', 'userdata')
     
-    def user_data_path(user_id = nil)
+    def user_data_path(account_id = nil)
       path = File.join(Filesystem::home_path, USER_DATA_PATH)
-      path = path + user_id unless user_id.nil?
+      path = path + account_id unless account_id.nil?
       File.join(*path)
     end
     
-    def game_data_path(user_id, game_id)
-      user_path = user_data_path(user_id)
+    def game_data_path(account_id, game_id)
+      user_path = user_data_path(account_id)
       File.join(user_path, game_id)
     end
     
